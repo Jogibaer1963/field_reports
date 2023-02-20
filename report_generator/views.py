27d892b350e781bae21d014691592f459a4da2ff
Jogibaer1963 <jrgnhsr@gmail.com>
2023-02-20 13:52:29 -0600

    Initial Commit

--- report_generator/views.py
+++ report_generator/views.py
@@ -9,6 +9,7 @@ from .forms import UploadFileForm
 def index(request):
     return render(request, 'report_generator/index.html')
 
+
 """
     row 0: Machine Number
     row 1: Operation Hours
@@ -37,8 +38,14 @@ def variable_view(request):
                     failure_long_text = str(row[6]) + str(row[7]) + str(row[8]) \
                                         + str(row[9]) + str(row[10]) + str(row[11])
                     diagnostic_long_text = str(row[12]) + str(row[13]) + str(row[14]) \
-                                        + str(row[15]) + str(row[16]) + str(row[17])
-                    print(failure_long_text, diagnostic_long_text)
-
+                                                        + str(row[15]) + str(row[16]) + str(row[17])
+                    remedy_long_text = str(row[18]) + str(row[19]) + str(row[20]) \
+                                                    + str(row[21]) + str(row[22]) + str(row[23])
+                    reason_long_text = str(row[24]) + str(row[25]) + str(row[26]) \
+                                                    + str(row[27]) + str(row[28])
+                    comment_long_text = str(row[29]) + str(row[30]) + str(row[31]) \
+                                                     + str(row[32]) + str(row[33]) + str(row[31]) \
+                                                     + str(row[32]) + str(row[33])
+                    print(failure_long_text, diagnostic_long_text, remedy_long_text, reason_long_text, comment_long_text)
 
     return render(request, 'report_generator/variable.html')
